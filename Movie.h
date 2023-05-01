c2f5f9a3c39418a60bbda36ac78668533c1bddbc
danielantebitechnion <103208886+danielantebitechnion@users.noreply.github.com>
2023-05-01 11:19:54 +0300

    Update Movie.h

---
+++ Movie.h
@@ -0,0 +1,19 @@
+#ifndef Movie_H
+#define Movie_H
+
+#include "wet1util.h"
+
+class Movie {
+    public:
+      int movieId;
+      Genre genre;
+      double rating; // avarage rating of the movie
+      int views; // total views for a movie
+      bool vipOnly; // define if only vip users can watch this movie    
+
+Movie() = default;
+Movie(int movieId, Genre genre, int views, bool vipOnly);
+~Movie() = default;
+
+};
+#endif // Movie_H
